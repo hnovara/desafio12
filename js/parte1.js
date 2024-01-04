00d3a3be551bcd3309dce95a5cb0bc10e676f134
Hernán Ezequiel Novara <her.novara@hotmail.com>
2024-01-03 23:54:18 -0300

    Ejercicio 3

--- js/parte1.js
+++ js/parte1.js
@@ -16,3 +16,19 @@ contenedor.appendChild(newSpan);
 // Color4: #39B54A ;
 
 // Ejercicio 3:
+let arreglo = ['#0074C1','#00A8FB','#FCD85E','#39B54A'];
+console.log(arreglo);
+
+let selectElement = document.createElement('select');
+
+arreglo.forEach(function(color) {
+  let optionElement = document.createElement('option');
+
+  optionElement.value = color;
+  optionElement.text = color;
+
+  selectElement.add(optionElement);
+});
+
+let contenedor2 = document.getElementById("container2");
+contenedor2.appendChild(selectElement);
