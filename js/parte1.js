a0b8d9ec0dab650a1034777c1e72af5f88607892
Hernán Ezequiel Novara <her.novara@hotmail.com>
2024-01-03 23:29:55 -0300

    Ejercicio 2

---
+++ js/parte1.js
@@ -0,0 +1,18 @@
+// Parte 1:
+
+// Ejercicio 1:
+let newSpan = document.createElement("span");
+newSpan.textContent = "Temática elegida: Juguetes";
+
+let contenedor = document.getElementById("container");
+contenedor.appendChild(newSpan);
+
+// Ejercicio 2:
+// Sitio web de referencia:   https://www.cebra.com.ar/
+// Paleta:
+// Color1: #0074C1 ;
+// Color2: #00A8FB ;
+// Color3: #FCD85E ;
+// Color4: #39B54A ;
+
+// Ejercicio 3:
